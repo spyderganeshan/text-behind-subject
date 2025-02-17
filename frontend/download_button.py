b58f701a2754d5cec61288a5c22aa8d5dcf156b3
ganeshan <ganeshanparthan@gmail.com>
2025-02-17 10:17:43 +0530

    fixed download error

--- frontend/download_button.py
+++ frontend/download_button.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from io import BytesIO
-from PIL import Image
 
 def convert_image_to_bytes(image):
     buf = BytesIO()
@@ -11,14 +10,10 @@ def download_image_button(processed_image):
     # Convert the processed image to bytes
     image_bytes = convert_image_to_bytes(processed_image)
 
-    # Initialize session state to store the image bytes
-    if "image_bytes" not in st.session_state:
-        st.session_state.image_bytes = image_bytes
-
     # Download button
     if st.download_button(
         label="Download Processed Image",
-        data=st.session_state.image_bytes,
+        data=image_bytes,
         file_name="processed_image.png",
         mime="image/png",
     ):
